a0453d886ddbb087a10b5b439c42b473ae71923a
bing.zhub <bing.zhub@gmail.com>
2018-08-24 23:39:28 +0800

    update textview

--- TextView/app/src/main/java/com/example/bing/textview/MainActivity.java
+++ TextView/app/src/main/java/com/example/bing/textview/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.bing.textview;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private Button mBtnTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBtnTextView = (Button)findViewById(R.id.btn_textView);
+
+        mBtnTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, TextViewActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
