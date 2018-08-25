a0cbbdf76949dc4108424ed8b4266f3b8e2282dd
bing.zhub <bing.zhub@gmail.com>
2018-08-25 23:19:19 +0800

    update button

--- TextView/app/src/main/java/com/example/bing/textview/MainActivity.java
+++ TextView/app/src/main/java/com/example/bing/textview/MainActivity.java
@@ -10,17 +10,27 @@ public class MainActivity extends AppCompatActivity {
 
 
     private Button mBtnTextView;
+    private Button mBtnButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         mBtnTextView = (Button)findViewById(R.id.btn_textView);
+        mBtnButton = (Button)findViewById(R.id.btn_button);
 
         mBtnTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, TextViewActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mBtnButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ButtonActivity.class);
                 startActivity(intent);
             }
         });
