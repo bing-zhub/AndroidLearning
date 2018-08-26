26ce1bdb08ed76fc69a67820bf6145f4cd4da892
bing.zhub <bing.zhub@gmail.com>
2018-08-26 22:43:58 +0800

    finish button chapter

--- TextView/app/src/main/java/com/example/bing/textview/ButtonActivity.java
+++ TextView/app/src/main/java/com/example/bing/textview/ButtonActivity.java
@@ -4,11 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class ButtonActivity extends AppCompatActivity {
 
     private Button mBtn3;
+    private TextView mTv8;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,12 +18,23 @@ public class ButtonActivity extends AppCompatActivity {
         setContentView(R.layout.activity_button);
 
         mBtn3 = (Button)findViewById(R.id.btn_3);
+        mTv8 = (TextView)findViewById(R.id.tv8);
+
         mBtn3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(ButtonActivity.this,"Hello World",Toast.LENGTH_LONG).show();
             }
         });
+
+        mTv8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ButtonActivity.this,"Hello World",Toast.LENGTH_LONG).show();
+            }
+        });
+
+
     }
 
     public void showToast(View view){
