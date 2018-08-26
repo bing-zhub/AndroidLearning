b1573c2158f734931e544101a064240ae1c43163
bing.zhub <bing.zhub@gmail.com>
2018-08-26 22:35:53 +0800

    update button

--- TextView/app/src/main/java/com/example/bing/textview/ButtonActivity.java
+++ TextView/app/src/main/java/com/example/bing/textview/ButtonActivity.java
@@ -2,12 +2,30 @@ package com.example.bing.textview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class ButtonActivity extends AppCompatActivity {
+
+    private Button mBtn3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_button);
+
+        mBtn3 = (Button)findViewById(R.id.btn_3);
+        mBtn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ButtonActivity.this,"Hello World",Toast.LENGTH_LONG).show();
+            }
+        });
     }
+
+    public void showToast(View view){
+        Toast.makeText(this,"Hello World",Toast.LENGTH_LONG).show();
+    }
+
 }
