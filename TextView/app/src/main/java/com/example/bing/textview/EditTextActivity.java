3064cec8294ece21b058fdd06a4810ef58342f60
bing.zhub <bing.zhub@gmail.com>
2018-08-26 23:18:54 +0800

    finish edit text

--- TextView/app/src/main/java/com/example/bing/textview/EditTextActivity.java
+++ TextView/app/src/main/java/com/example/bing/textview/EditTextActivity.java
@@ -2,24 +2,51 @@ package com.example.bing.textview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class EditTextActivity extends AppCompatActivity {
 
     private Button btnLogin;
+    private EditText etUsername;
+    private EditText etPassword;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_text);
 
         btnLogin = (Button)findViewById(R.id.btn_login);
+        etUsername = (EditText)findViewById(R.id.et_1);
+        etPassword = (EditText)findViewById(R.id.et_2);
+
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(EditTextActivity.this, "登录成功", Toast.LENGTH_LONG);
+            }
+        });
+
+        etUsername.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                Log.d("EditText", s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
             }
         });
     }
