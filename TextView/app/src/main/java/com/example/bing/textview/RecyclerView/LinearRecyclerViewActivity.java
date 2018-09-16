58a82ad3094dd130889c302bb88ba7f59aee67d1
bing.zhub <bing.zhub@gmail.com>
2018-09-16 23:28:56 +0800

    update linear recycler view

---
+++ TextView/app/src/main/java/com/example/bing/textview/RecyclerView/LinearRecyclerViewActivity.java
@@ -0,0 +1,15 @@
+package com.example.bing.textview.RecyclerView;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.bing.textview.R;
+
+public class LinearRecyclerViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linear_recycler_view);
+    }
+}
---
+++ TextView/app/src/main/java/com/example/bing/textview/RecyclerView/RecyclerViewActivity.java
@@ -0,0 +1,29 @@
+package com.example.bing.textview.RecyclerView;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.bing.textview.R;
+
+public class RecyclerViewActivity extends AppCompatActivity {
+
+    private Button mBtnLinear;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycle_view);
+        mBtnLinear = findViewById(R.id.btn_linear);
+
+        mBtnLinear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RecyclerViewActivity.this, LinearRecyclerViewActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
