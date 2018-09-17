54241698fc299033efaa828c3d1bbe04366e8985
bing.zhub <bing.zhub@gmail.com>
2018-09-17 23:40:46 +0800

    finish recycler view implementation

--- TextView/app/src/main/java/com/example/bing/textview/RecyclerView/LinearRecyclerViewActivity.java
+++ TextView/app/src/main/java/com/example/bing/textview/RecyclerView/LinearRecyclerViewActivity.java
@@ -2,14 +2,22 @@ package com.example.bing.textview.RecyclerView;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.example.bing.textview.R;
 
 public class LinearRecyclerViewActivity extends AppCompatActivity {
 
+    private RecyclerView mRvMain;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_linear_recycler_view);
+        mRvMain = findViewById(R.id.rv_main);
+        mRvMain.setLayoutManager(new LinearLayoutManager(LinearRecyclerViewActivity.this));
+
     }
 }
