ec4818083505eea8feb3676c80fee219167718a0
bing.zhub <bing.zhub@gmail.com>
2018-09-29 21:05:19 +0800

    finish navigation demo

--- NavigationDemo/app/src/main/java/com/example/bing/navigationdemo/MainFragment.kt
+++ NavigationDemo/app/src/main/java/com/example/bing/navigationdemo/MainFragment.kt
@@ -6,23 +6,24 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import kotlinx.android.synthetic.main.fragment_main.*
 
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- *
- */
 class MainFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        button1.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.to_fragment1))
+        button2.setOnClickListener{
+            it.findNavController().navigate(R.id.fragment2)
+        }
     }
 
 
---
+++ NavigationDemo/app/src/main/java/com/example/bing/navigationdemo/fragment1.kt
@@ -0,0 +1,41 @@
+package com.example.bing.navigationdemo
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.findNavController
+import kotlinx.android.synthetic.main.fragment_fragment1.*
+
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+class fragment1 : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fragment1, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        nextBtn.setOnClickListener {
+            val stringBundle = Bundle()
+            stringBundle.putString("string", textEdit.text.toString())
+            it.findNavController().navigate(R.id.to_fragment3, stringBundle)
+        }
+    }
+
+
+}
