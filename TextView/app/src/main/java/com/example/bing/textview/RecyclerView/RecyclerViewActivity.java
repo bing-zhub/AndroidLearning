9db309b7acd00a51232b22c197fb70bb00a6c192
bing.zhub <bing.zhub@gmail.com>
2018-10-11 22:59:32 +0800

    add horizental recycler view

--- TextView/app/src/main/java/com/example/bing/textview/RecyclerView/RecyclerViewActivity.java
+++ TextView/app/src/main/java/com/example/bing/textview/RecyclerView/RecyclerViewActivity.java
@@ -10,18 +10,27 @@ import com.example.bing.textview.R;
 
 public class RecyclerViewActivity extends AppCompatActivity {
 
-    private Button mBtnLinear;
+    private Button mBtnLinear,mBtnHor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycle_view);
         mBtnLinear = findViewById(R.id.btn_linear);
+        mBtnHor = findViewById(R.id.btn_hor);
 
         mBtnLinear.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(RecyclerViewActivity.this, LinearRecyclerViewActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mBtnHor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RecyclerViewActivity.this, HorRecyckerViewActivity.class);
                 startActivity(intent);
             }
         });
