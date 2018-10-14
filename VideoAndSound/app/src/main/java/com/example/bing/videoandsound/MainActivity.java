8af78b5c68f775e40d3119a0554a36d933921764
bing.zhub <bing.zhub@gmail.com>
2018-10-14 16:52:11 +0800

    add a video view

--- VideoAndSound/app/src/main/java/com/example/bing/videoandsound/MainActivity.java
+++ VideoAndSound/app/src/main/java/com/example/bing/videoandsound/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.bing.videoandsound;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        VideoView videoView;
+        videoView = findViewById(R.id.videoView);
+        videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.demovideo);
+        videoView.start();
     }
 }
