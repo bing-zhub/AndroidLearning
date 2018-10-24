b736838797d246b4bc2970e39e5613d0734044e2
bing.zhub <bing.zhub@gmail.com>
2018-10-24 23:01:12 +0800

    use sqlite

--- SomeStuff/app/src/main/java/com/example/bing/somestuff/SqliteActivity.java
+++ SomeStuff/app/src/main/java/com/example/bing/somestuff/SqliteActivity.java
@@ -4,6 +4,8 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class SqliteActivity extends AppCompatActivity {
 
@@ -12,22 +14,21 @@ public class SqliteActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sqlite);
 
-        SQLiteDatabase myDataabse = this.openOrCreateDatabase("User", MODE_PRIVATE, null);
-        myDataabse.execSQL("CREATE TABLE IF NOT EXISTS user (name VARCHAR, age INT(3))");
+        SQLiteDatabase myDatabase = this.openOrCreateDatabase("newUser", MODE_PRIVATE, null);
+        myDatabase.execSQL("CREATE TABLE IF NOT EXISTS newUser (name VARCHAR, age INT(3), id INTEGER PRIMARY KEY)");
+        // 与SQL完全一样
+        myDatabase.execSQL("INSERT INTO newUser(name, age) values('nike', 20)");
+        myDatabase.execSQL("INSERT INTO newUser(name, age) values('ada', 22)");
 
-        myDataabse.execSQL("INSERT INTO User(name, age) values('nike', 20)");
-        myDataabse.execSQL("INSERT INTO User(name, age) values('ada', 22)");
-
-        Cursor c = myDataabse.rawQuery("SELECT * FROM User ", null);
+        Cursor c = myDatabase.rawQuery("SELECT * FROM newUser ", null);
         int nameIndex = c.getColumnIndex("name");
         int ageIndex = c.getColumnIndex("age");
 
         c.moveToFirst();
 
-        while(c!=null){
-            System.out.println(c.getString(nameIndex));
-            System.out.println(c.getInt(ageIndex));
-
+        while(!c.isAfterLast()){
+            Log.i("result",c.getString(nameIndex)+" "+c.getString(ageIndex));
+            Log.i("result", " --------------");
             c.moveToNext();
         }
     }
