da3d93226b0be7225e2ac57c301fa9af9c0626b8
bing.zhub <bing.zhub@gmail.com>
2018-12-03 21:48:52 +0800

    udpate starter page

--- YiJi/app/src/main/java/com/example/bing/yiji/StarterActivity.java
+++ YiJi/app/src/main/java/com/example/bing/yiji/StarterActivity.java
@@ -1,108 +1,69 @@
 package com.example.bing.yiji;
 
-import android.annotation.SuppressLint;
-import android.app.ActionBar;
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.MotionEvent;
-import android.view.View;
-import android.view.animation.AlphaAnimation;
-import android.view.animation.Animation;
+import android.os.SystemClock;
+import android.widget.ImageView;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+import rx.Observable;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
 
 public class StarterActivity extends Activity {
-    private static final int UI_ANIMATION_DELAY = 300;
-    private final Handler mHideHandler = new Handler();
-    private View mContentView;
-    private final Runnable mHidePart2Runnable = new Runnable() {
-        @SuppressLint("InlinedApi")
-        @Override
-        public void run() {
-            mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-                    | View.SYSTEM_UI_FLAG_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-        }
-    };
-    private final Runnable mShowPart2Runnable = new Runnable() {
-        @Override
-        public void run() {
-            ActionBar actionBar = getActionBar();
-            if (actionBar != null) {
-                actionBar.show();
-            }
-        }
-    };
-    private final Runnable mHideRunnable = new Runnable() {
-        @Override
-        public void run() {
-            hide();
-        }
-    };
+    ImageView mIVEntry;
+
+    private static final int ANIM_TIME = 2000;
+
+    private static final float SCALE_END = 1.15F;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_starter);
+        mIVEntry = findViewById(R.id.iv_entry);
 
-        mContentView = findViewById(R.id.fullscreen_content);
+        mIVEntry.setImageResource(R.drawable.launch_1);
 
-        AlphaAnimation anim = new AlphaAnimation(0f, 1f);
-        anim.setDuration(3000);
-        anim.setAnimationListener(new Animation.AnimationListener() {
+        Observable.timer(1000, TimeUnit.MILLISECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Action1<Long>()
+                {
+
+                    @Override
+                    public void call(Long aLong)
+                    {
+                        startAnim();
+                    }
+                });
+    }
+
+    private void startAnim() {
+
+        ObjectAnimator animatorX = ObjectAnimator.ofFloat(mIVEntry, "scaleX", 1f, SCALE_END);
+        ObjectAnimator animatorY = ObjectAnimator.ofFloat(mIVEntry, "scaleY", 1f, SCALE_END);
+
+        AnimatorSet set = new AnimatorSet();
+        set.setDuration(ANIM_TIME).play(animatorX).with(animatorY);
+        set.start();
+
+        set.addListener(new AnimatorListenerAdapter()
+        {
 
             @Override
-            public void onAnimationStart(Animation arg0) {
+            public void onAnimationEnd(Animator animation)
+            {
 
-            }
-
-            @Override
-            public void onAnimationRepeat(Animation arg0) {
-
-            }
-
-            @Override
-            public void onAnimationEnd(Animation arg0) {
                 startActivity(new Intent(StarterActivity.this, GuideActivity.class));
-                finish();
+                StarterActivity.this.finish();
             }
         });
-         mContentView.startAnimation(anim);
-
-    }
-
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-
-        delayedHide(100);
-    }
-
-    private void hide() {
-        ActionBar actionBar = getActionBar();
-        if (actionBar != null) {
-            actionBar.hide();
-        }
-
-        mHideHandler.removeCallbacks(mShowPart2Runnable);
-        mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY);
-    }
-
-    @SuppressLint("InlinedApi")
-    private void show() {
-        mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
-
-        mHideHandler.removeCallbacks(mHidePart2Runnable);
-        mHideHandler.postDelayed(mShowPart2Runnable, UI_ANIMATION_DELAY);
-    }
-
-    private void delayedHide(int delayMillis) {
-        mHideHandler.removeCallbacks(mHideRunnable);
-        mHideHandler.postDelayed(mHideRunnable, delayMillis);
     }
 }
